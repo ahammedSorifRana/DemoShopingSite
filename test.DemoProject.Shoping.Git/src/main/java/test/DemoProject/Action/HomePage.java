3854d0a0a405001447707a54d84962a003628b35
ahammedsorif <ahammedsorif@ahammeds-air.home>
2017-10-30 09:55:07 -0400

    mac-final

--- test.DemoProject.Shoping.Git/src/main/java/test/DemoProject/Action/HomePage.java
+++ test.DemoProject.Shoping.Git/src/main/java/test/DemoProject/Action/HomePage.java
@@ -1,6 +1,5 @@
 package test.DemoProject.Action;
 
-import org.apache.poi.util.SystemOutLogger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
